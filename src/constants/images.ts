46eeded9aa10e656f0ded19d883d583c544eba1d
minji0629 <hmj9455@naver.com>
2026-08-09 15:03:53 +0900

    Update images.ts

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,5 +1,4 @@
-export const LANDING_BG = '/u4332239835_an_anime_style_digital_painting_of_a_coastal_town_fb0cc363-f21a-4d77-886d-43abdb5daa65_2.png';
-
+export const LANDING_BG = '/landing-bg.png
 export const FOREST_BG =
   'https://images.pexels.com/photos/1671325/pexels-photo-1671325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=1980&dpr=1';
 
